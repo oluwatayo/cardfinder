390f290c1baa50600f9b622697d07bd5dabce3a1
itkala <32441611+itkala@users.noreply.github.com>
2017-10-03 08:07:43 +0100

    Update Urls.py

--- LostCard/urls.py
+++ LostCard/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'lostcard'
 
 urlpatterns = [
     #/lostcard/
-    url(r'^$', views.home, name='home'),
+    url(r'^$', TemplateView.as_view(template_name = "LostCard/index1.html"), name='home'),
 
     url(r'^send/mail/', views.mail, name='mail'),
 
